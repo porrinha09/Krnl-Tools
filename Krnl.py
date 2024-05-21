da831c07e3bdb251f64722c8c25f64ddcc68cea4
porrinha09 <142546604+porrinha09@users.noreply.github.com>
2024-05-21 04:29:31 -0300

    Atualizar o Krnl.py

--- Krnl.py
+++ Krnl.py
@@ -1,4 +1,13 @@
-#!/usr/bin/env python
-
-# Limpar o console
 print("\033c")
+
+COR = "\033[38;2;92;92;255m"
+BLUE = "\033[38;2;128;163;255m"
+VERDE = "\033[38;2;79;205;177m"
+VERMELHO = "\033[38;2;255;0;0m"  
+LARANJA = "\033[38;2;241;133;30m"
+RESET = "\033[0m"
+
+print(BLUE + "Bem-vindo ao Krnl Tools" + RESET)
+print(BLUE + "------------------------------------------------" + RESET)
+print(LARANJA + "IP:" + BLUE)
+print("{1} - ;my ip")
