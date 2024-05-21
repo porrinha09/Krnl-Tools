260ac45f2d19597d58491f2b02ca2ef82229399e
porrinha09 <142546604+porrinha09@users.noreply.github.com>
2024-05-21 05:27:08 -0300

    Atualizar o Krnl.py

--- Krnl.py
+++ Krnl.py
@@ -20,48 +20,37 @@ def print_command_list():
     
 def process_command(command):
     if command == ";my ip":
-    	response = requests.get('https://ipinfo.io/ip')
-    return response.text.strip()
+        response = requests.get('https://ipinfo.io/ip')
+        return response.text.strip()
 
-ip = get_ip()
-print(f"Seu IP: {ip}")
     if command == ";ip info":
-    	def get_ip_info(ip):
-    url = f"https://ipinfo.io/{ip}"
-    response = requests.get(url)
-    if response.status_code == 200:
-        data = json.loads(response.text)
-        return data
-    else:
-        return None
+        def get_ip_info(ip):
+            url = f"https://ipinfo.io/{ip}"
+            response = requests.get(url)
+            if response.status_code == 200:
+                data = json.loads(response.text)
+                return data
+            else:
+                return None
 
-def get_current_time(timezone):
-    import datetime
-    import pytz
-    tz = pytz.timezone(timezone)
-    current_time = datetime.datetime.now(tz)
-    return current_time.strftime("%H:%M")
+        ip = input("IP: ")
+        ip_info = get_ip_info(ip)
 
-ip = input("IP: ")
-ip_info = get_ip_info(ip)
+        if ip_info:
+            country = ip_info.get('country', 'N/A')
+            state = ip_info.get('region', 'N/A')
+            city = ip_info.get('city', 'N/A')
+            timezone = ip_info.get('timezone', 'N/A')
 
-if ip_info:
-    country = ip_info.get('country', 'N/A')
-    state = ip_info.get('region', 'N/A')
-    city = ip_info.get('city', 'N/A')
-    timezone = ip_info.get('timezone', 'N/A')
+            print(f"País: {country}")
+            print(f"Estado: {state}")
+            print(f"Cidade: {city}")
+            print(f"Horário: {timezone}")
+        else:
+            print("Erro ao obter informações do IP.")
 
-    current_time = get_current_time(timezone)
-
-    print(f"País: {country}")
-    print(f"Estado: {state}")
-    print(f"Cidade: {city}")
-    print(f"Horário: {current_time}")
-else:
-    print("Erro ao obter informações do IP.")
-
-os.system("clear")
-print_command_list()
+        os.system("clear")
+        print_command_list()
 
 while True:
     command = input("\033[34m┌──(Kelvin zv)-[~]\n└─$ \033[0m")
