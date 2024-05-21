482cbeb57c4a36053494e3e4cbc921ce1803a965
porrinha09 <142546604+porrinha09@users.noreply.github.com>
2024-05-21 04:57:53 -0300

    Atualizar o Krnl.py

--- Krnl.py
+++ Krnl.py
@@ -1,4 +1,4 @@
-print("\033c")
+import os
 
 COR = "\033[38;2;92;92;255m"
 BLUE = "\033[38;2;128;163;255m"
@@ -7,10 +7,23 @@ VERMELHO = "\033[38;2;255;0;0m"
 LARANJA = "\033[38;2;241;133;30m"
 RESET = "\033[0m"
 
-while True:
-    input("\033[34m┌──(Kelvin zv)-[~]\n└─$ \033[0m")
+def process_command(command):
+    if command == "my ip":
+        # Adicione aqui a lógica para obter e exibir o endereço IP
+        print("Seu endereço IP é 192.168.1.1")  # Exemplo simples
+    else:
+        print("Comando não reconhecido")
+
 print(BLUE + "Bem-vindo ao Krnl Tools" + RESET)
 print(BLUE + "------------------------------------------------" + RESET)
 print(LARANJA + "IP:" + BLUE)
 print("{1} - ;my ip")
 
+while True:
+    os.system("clear")
+    print(BLUE + "Bem-vindo ao Krnl Tools" + RESET)
+    print(BLUE + "------------------------------------------------" + RESET)
+    print(LARANJA + "IP:" + BLUE)
+    print("{1} - ;my ip")
+    command = input("\033[34m┌──(Kelvin zv)-[~]\n└─$ \033[0m")
+    process_command(command)
